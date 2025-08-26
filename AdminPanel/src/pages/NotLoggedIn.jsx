d3f15562f8250d5b6158836e2076b2b374ea2f32
ram varma <ramchandra010304@gmail.com>
2025-08-26 14:57:55 +0530

    styling improved

--- AdminPanel/src/pages/NotLoggedIn.jsx
+++ AdminPanel/src/pages/NotLoggedIn.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LockTwoToneIcon from '@mui/icons-material/LockTwoTone';
 
-
 function NotLoggedIn() {
   return (
     <div
@@ -15,17 +14,26 @@ function NotLoggedIn() {
         style={{
           textAlign: "center",
           padding: "40px",
+          // padding: "5%",
           borderRadius: "16px",
-          backgroundColor: "#fff",
+          // backgroundColor: "#fff",
+          backgroundColor: "#2D2D3F",
           maxWidth: "400px",
           width: "100%",
+          boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
         }}
       >
         <LockTwoToneIcon style={{ fontSize: "120px", color: "#ff4d4f", marginBottom: "20px"}} />
-        <h1 style={{ marginBottom: "5px", fontSize: "28px", color: "#333" }}>
+        <h1 style={{ marginBottom: "5px", fontSize: "28px",
+          //  color: "#333" 
+          //  color: "#7f8c8d" 
+           }}>
           Access Denied
         </h1>
-        <p style={{ marginBottom: "20px", fontSize: "16px", color: "#666" }}>
+        <p style={{ marginBottom: "20px", fontSize: "16px", 
+          // color: "#666" 
+          color: "#7f8c8d" 
+          }}>
           You need to be logged in to access this page
         </p>
         <Link
@@ -34,14 +42,16 @@ function NotLoggedIn() {
             display: "inline-block",
             padding: "12px 24px",
             borderRadius: "8px",
-            backgroundColor: "#007bff",
+            // backgroundColor: "#007bff",
+            backgroundColor: "#7c4dff",
             color: "#fff",
             textDecoration: "none",
             fontWeight: "600",
             transition: "background 0.3s ease",
           }}
           onMouseOver={(e) => (e.target.style.backgroundColor = "#0056b3")}
-          onMouseOut={(e) => (e.target.style.backgroundColor = "#007bff")}
+          // onMouseOut={(e) => (e.target.style.backgroundColor = "#007bff")}
+          onMouseOut={(e) => (e.target.style.backgroundColor = "#7c4dff")}
         >
           Go to Login
         </Link>
